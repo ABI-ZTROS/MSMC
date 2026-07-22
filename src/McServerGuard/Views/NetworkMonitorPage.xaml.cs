8791a8918558af9eba1f17007189771cd91f6187
ABI-ZTROS <ABI-ZTROS@users.noreply.github.com>
2026-07-22 13:06:40 +0000

    feat: 优化服务器检测与配置
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/McServerGuard/Views/NetworkMonitorPage.xaml.cs
+++ src/McServerGuard/Views/NetworkMonitorPage.xaml.cs
@@ -1,5 +1,8 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.ComponentModel;
 using McServerGuard.ViewModels;
 
 namespace McServerGuard.Views;
@@ -7,6 +10,7 @@ namespace McServerGuard.Views;
 public partial class NetworkMonitorPage : UserControl
 {
     private NetworkMonitorViewModel? _viewModel;
+    private Storyboard? _refreshStoryboard;
 
     public NetworkMonitorPage()
     {
@@ -18,11 +22,58 @@ public partial class NetworkMonitorPage : UserControl
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
         _viewModel = DataContext as NetworkMonitorViewModel;
+        if (_viewModel != null)
+        {
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+            if (_viewModel.IsRefreshing)
+                StartRefreshAnimation();
+        }
     }
 
     private void OnUnloaded(object sender, RoutedEventArgs e)
     {
+        if (_viewModel != null)
+            _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
+
+        StopRefreshAnimation();
         (_viewModel as IDisposable)?.Dispose();
         _viewModel = null;
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(NetworkMonitorViewModel.IsRefreshing) && _viewModel != null)
+        {
+            if (_viewModel.IsRefreshing)
+                StartRefreshAnimation();
+            else
+                StopRefreshAnimation();
+        }
+    }
+
+    private void StartRefreshAnimation()
+    {
+        if (_refreshStoryboard != null)
+            return;
+
+        _refreshStoryboard = new Storyboard();
+        var anim = new DoubleAnimation
+        {
+            From = 0,
+            To = 360,
+            Duration = TimeSpan.FromSeconds(1),
+            RepeatBehavior = RepeatBehavior.Forever
+        };
+        Storyboard.SetTarget(anim, RefreshRotateTransform);
+        Storyboard.SetTargetProperty(anim, new PropertyPath(RotateTransform.AngleProperty));
+        _refreshStoryboard.Children.Add(anim);
+        _refreshStoryboard.Begin();
+    }
+
+    private void StopRefreshAnimation()
+    {
+        _refreshStoryboard?.Stop();
+        _refreshStoryboard = null;
+        RefreshRotateTransform.Angle = 0;
     }
 }
