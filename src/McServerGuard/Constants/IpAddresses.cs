03ee5101ab049fc19887e1e3eafb81fd54ae03c4
ABI-ZTROS <ABI-ZTROS@users.noreply.github.com>
2026-07-22 02:36:40 +0000

    feat: add IpAddresses constants

---
+++ src/McServerGuard/Constants/IpAddresses.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace McServerGuard.Constants;
+
+public static class IpAddresses
+{
+    public static readonly List<string> All =
+    [
+        "127.0.0.1",
+        "0.0.0.0",
+        "::1",
+        "::"
+    ];
+}
